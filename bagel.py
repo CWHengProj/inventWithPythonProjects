9838e1d540fe1f388e7d9544f25801724b28a756
CWHengProj <142023227+CWHengProj@users.noreply.github.com>
2023-11-09 12:56:21 +0800

    added more functions, left with the comparator before completion of first draft

--- bagel.py
+++ bagel.py
@@ -8,23 +8,31 @@
 from random import randint
 def main():
     coolIntroScreen()
-    attempts=0
-    answer=randint(1,999)
-    print(answer) #to be removed
+    attempts,answer=freshStart()
     while True:
         userInput=input('Please enter your guesss from 0 to 999:')
         if inputValidator(userInput)== True:
-            comparator(formatter(answer,userInput))
+            if comparator(formatter(answer,userInput)):
+                victoryScreen()
+                exit()
+            attempts+=1
+            if attempts==difficultyGenerator():
+                if gameOver()==True:
+                    freshStart()
+                    continue
+                else:
+                    print('Thank you for playing. See you again!')
+                    exit()
             break
-
-    
-    victoryScreen()
 def coolIntroScreen():
     #explains the rules to the player
     print("Welcome! This is a deductive logic game, where you have to guess a digit from 0 to 999.\n If the correct digit is in the wrong position, it will be highlighted in red.\n If the correct digit is in the correct position, it will be highlighted in green.\n Else, the digits will appear as white.\n Now that we're clear on the rules, lets begin!")
-
-
-
+def freshStart():
+    #initializes the values again
+    attempts=0
+    answer=randint(1,999)
+    print(answer) #to be removed
+    return attempts, answer
 def inputValidator(x):
     #ensure that input is within range and numerical
     try:
@@ -33,20 +41,32 @@ def inputValidator(x):
     except ValueError:
         print('Please enter an appropriate value from 0 to 999')
         return False
-def formatter(ans,input):
-    #creates a 3 digit format for both answer and user input
-    ansFormatted =f'{ans:02d}'
-    guessFormatter =f'{input:02d}'
-    return ansFormatted, guessFormatter
-def comparator(a,b):
-    #creates a comparator to give the user hints with color code
-    
-"""def gameOver():
+def gameOver(attempts):
     #counts the number of attempts the user has made and serves a game over screen all attempts used up
+    print(f'Game over! You have used up all {attempts} attempts!')
+    response =(input('Would you like to play again? Y/N'))
+    if response =='y' or response =='Y' or response =='Yes' or response =='yes':
+        return True
+    return False
 def difficultyGenerator():
-    #sets the number of attempts before game over"""
+    #sets the number of attempts before game over
+    return 10
 def victoryScreen():
     #congratulates user once complete, returns number of attempts as well
     print('hiphiphooray!')
+def formatter(ans,input):
+    #creates a 3 digit format for both answer and user input
+    ansFormatted =(f'{ans:02d}').split()
+    guessFormatter =(f'{input:02d}').split()
+    return ansFormatted, guessFormatter
+def comparator(a,b):
+    #creates a comparator to give the user hints with color code
+    for i in range(len(a)):
+        if b[i]==a[i]:
+            #turn the value green
+        if b[i]!=a[i] and b[i] in a:
+            #turn the value orange
+    if a=b:
+        return True
 if __name__=="__main__":
     main()
